aca4838c5904747d56b2289a8d04e648bcc1bb31
Tim Maina <oneuptim@Tims-MacBook-Pro.local>
2017-05-01 00:31:55 -0600

    Implemented Rental Check Request feature

--- src/pages/rental-check/rental-check.ts
+++ src/pages/rental-check/rental-check.ts
@@ -24,6 +24,16 @@ export class RentalCheckPage {
 
   loggedUser:string = '';
   user = {};
+  rentalHistory: any[] = [];
+  pmFirstName:string = '';
+  pmLastName:string = '';
+  email:string = '';
+  phoneNumber:string = '';
+  applicantsEmail = '';
+  agencyEmail = '';
+  applicantID = '';
+  propertyAddress = '';
+
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private backand: BackandService, private alertController: AlertController, private toastCtrl: ToastController, public http: Http, public loadingCtrl: LoadingController, public actionSheetCtrl: ActionSheetController, public viewCtrl: ViewController) {
 
@@ -38,15 +48,59 @@ export class RentalCheckPage {
     .then(res => {
       this.user = res.data
       this.loggedUser = res.data.userId
-      console.log(res.data, "<==== 15. RENTAL CHECKS GET USER DETAILS");
+
+      this.backand.object.getOne("users", this.loggedUser, {
+        "deep" : true })
+        .then(res => {
+          this.email = res.data.email
+          this.rentalHistory = res.data.rentalhistory
+      })
+      .catch(err => {
+        console.log(err);
+      }); // End of user object fetch
+
     })
     .catch(err => {
       console.log(err);
     }); // End of user object fetch
 
+  }// END CONSTUCTOR
 
+// Submit rental check reqeust
+  requestRentalCheck(id) {
+  this.backand.object.getOne("rentalHistory", id, {
+    "deep" : true })
+    .then(res => {
+      this.propertyAddress = id
+      this.applicantsEmail = this.email
+      this.agencyEmail = res.data.email
+      this.applicantID = this.loggedUser
 
-  }
+      let options = {
+        returnObject: true
+      }
+
+      let data = {
+        'propertyAddress': this.propertyAddress, 'applicantsEmail': this.applicantsEmail, 'agencyEmail': this.agencyEmail, 'applicantID': this.loggedUser
+      }
+      this.backand.object.create('tenantCheck', data, options)
+      .then(res => {
+        console.log(res.data, "created object")
+       alert('Rental Check Successfully Submitted');
+      //  this.dismiss();
+
+      })
+      .catch(error => {
+       console.log(error, '<===== data from backend save handler')
+      })
+
+  })
+  .catch(err => {
+    console.log(err);
+  }); // End of user object fetch
+
+} // END OF REQUEST METHOD
+
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RentalCheckPage');
